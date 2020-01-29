1cec12e5fbd2335a1ed8179fb3b6ec4596c9f100
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-01-29 22:57:11 +0100

    first

---
+++ src/doa/ConnectionDB.java
@@ -0,0 +1,12 @@
+package doa;
+
+import java.sql.Connection;
+
+public class ConnectionDB {
+    String url = "jdbc:mysql://localhost/ENSIAS_WIKI" ;
+    Connection connexion = null ;
+    try {
+        connexion =
+    }
+
+}
